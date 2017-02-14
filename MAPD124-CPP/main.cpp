3123997836814bb43701dfcf775f1a444c6073ab
Basil <basiltalias92@gmail.com>
2017-02-14 18:56:11 -0500

    Initial Commit

---
+++ MAPD124-CPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MAPD124-CPP
+//
+//  Created by Basil on 2017-02-14.
+//  Copyright © 2017 Centennial College. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
